2fcf779ba93aaa958f331acfbe73fa6e1e6494c6
cl <chunrufenglin@outlook.com>
2024-10-14 19:57:23 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace gas_sensor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
