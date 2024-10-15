104a9da62f0b3867383e5cc7ff019d48fac6760e
cl <zhangchunlin2023@outlook.com>
2024-10-15 16:54:52 +0800

    气体参数蓝编写完成， 获取可用串口部分代码完成 且已绑定

---
+++ ViewModel/mainViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.IO.Ports;
+
+namespace gas_sensor.ViewModel
+{
+    partial class mainViewModels : ObservableObject
+    {
+        public ObservableCollection<string> AvailablePorts { get; } = new ObservableCollection<string>(SerialPort.GetPortNames());
+    }
+}
