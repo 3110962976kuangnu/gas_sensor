745dfb6739654d91049db253e8aaca0aa93b35c5
cl <chunrufenglin@outlook.com>
2024-10-15 20:27:07 +0800

    编写数据模型

---
+++ Model/GasSensorModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gas_sensor.Model
+{
+    class GasSensorModel
+    {
+        public UInt32 Sensor_board_status { get; set; } = 0;
+        public float Gas_concentration_float { get; set; }
+        public UInt32 Gas_concentration_int { get; set; }
+        public UInt16 Gas_adc_value { get; set; }
+        public UInt16 temperature_int { get; set; }
+        public UInt16 temperature_adc_value { get; set; }
+        public enum Enum_GasType
+        {
+            GasType_O2 = 2,
+            GasType_CH4 = 3,
+        }
+        public Enum_GasType GasType { get; set; }
+        public byte sensor_id { get; set; }
+        public bool Is_toxic_gas { get; set; }
+        public bool Enable_alarm1 { get; set; }
+        public bool Enable_alarm2 { get; set; }
+        public enum Enum_normal_operating_range
+        {
+            Operating_range_low_range = 0,
+            Operating_range_middle_range = 1,
+            Operating_range_high_range = 2,
+        }
+        public Enum_normal_operating_range Normal_Operating_Range { get; set; }
+        public enum Enum_display_resolution
+        {
+            display_resolution_None = 0,
+            display_resolution_One = 1,
+            display_resolution_Two = 2,
+        }
+        public Enum_display_resolution Display_Resolution { get; set; }
+        public UInt32 Gas_full_range { get; set; }
+        public UInt32 alarm1_value { get; set; }
+        public UInt32 alarm2_value { get; set; }
+        public UInt16 Warm_up_time { get; set; }
+        public bool Enable_zero_display { get; set; }
+        public UInt32 bd0_value { get; set; }
+    }
+}
--- ViewModel/mainViewModels.cs
+++ ViewModel/mainViewModels.cs
@@ -6,11 +6,40 @@ using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.IO.Ports;
+using gas_sensor.Model;
+using CommunityToolkit.Mvvm.Input;
 
 namespace gas_sensor.ViewModel
 {
     partial class mainViewModels : ObservableObject
     {
-        public ObservableCollection<string> AvailablePorts { get; } = new ObservableCollection<string>(SerialPort.GetPortNames());
+        [ObservableProperty]
+        private GasSensorModel gas_model;
+
+        [ObservableProperty]
+        private int select_port_index = 0;
+
+        [ObservableProperty]
+        private int slave_id = 0xfe;
+
+        [ObservableProperty]
+        private SerialPort sp;
+
+        public ObservableCollection<string> AvailablePorts { get; set; } = new ObservableCollection<string>(SerialPort.GetPortNames());
+        [RelayCommand]
+        void update_available_ports()
+        {
+            AvailablePorts.Clear();
+            var portNames = SerialPort.GetPortNames(); // 获取所有端口名称
+            foreach (var port in portNames)
+            {
+                AvailablePorts.Add(port); // 将每个端口名称添加到 ObservableCollection 中
+            }
+        }
+
+        public mainViewModels()
+        {
+            gas_model = new GasSensorModel();
+        }
     }
 }
