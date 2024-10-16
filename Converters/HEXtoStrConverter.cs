00001e70f4162bc22b8540cfb6236fc21486f4ad
cl <zhangchunlin2023@outlook.com>
2024-10-16 17:08:32 +0800

    新增HEXtoStrConverter转换器，优化数据格式处理
    - 添加HEXtoStrConverter类，实现int到十六进制字符串的转换及反向转换
    - 在MainWindow.xaml中注册新转换器，并应用到Slave_id的显示格式化
    - 修改部分UI元素的Text属性，优化数据显示效果

---
+++ Converters/HEXtoStrConverter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace gas_sensor.Converters
+{
+    internal class HEXtoStrConverter : IValueConverter
+    {
+        private int? lastValidValue;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int v)
+            {
+                return $"{v:X}";
+            }
+            return " value not int";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string s)
+            {
+                if (s.StartsWith("0x") || s.StartsWith("0X"))
+                {
+                    s = s.Substring(2);
+                }
+                if (s.Length > 2 || s.Length == 0)
+                {
+                    return lastValidValue ?? DependencyProperty.UnsetValue;
+                }
+
+                // 尝试将十六进制字符串转换为整数
+                if (int.TryParse(s, NumberStyles.HexNumber, culture, out int result))
+                {
+                    lastValidValue = result; // 更新上一次有效值
+                    return result;
+                }
+            }
+            // 返回上一次有效值（如果存在），否则返回未设置值
+            return lastValidValue ?? DependencyProperty.UnsetValue;
+        }
+    }
+
+    internal class GetBitBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (
+                value is int intValue
+                && parameter is string bitString
+                && int.TryParse(bitString, out int bitindex))
+            {
+                bool bitvalue = (intValue & (1 << bitindex)) != 0;
+                return bitvalue;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolvalue
+                && parameter is string bitString
+                && int.TryParse(bitString, out int bitindex))
+            {
+                int res = boolvalue ? (1 << bitindex) : 0;
+                return res;
+            }
+            return 0;
+        }
+    }
+
+
+}
