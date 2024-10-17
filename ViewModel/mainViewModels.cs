0205d01920fc2f2ebda5658d4851133ce09f35e1
cl <chunrufenglin@outlook.com>
2024-10-17 20:33:10 +0800

    实时状态部分 属性编写 并完成绑定

--- ViewModel/mainViewModels.cs
+++ ViewModel/mainViewModels.cs
@@ -53,22 +53,95 @@ namespace gas_sensor.ViewModel
             gas_model = new GasSensorModel();
         }
 
+        #region REG_40000 通道状态
+
+        static bool get_bit_bool(UInt32 reg, int index)
+        {
+            return (reg & (1 << index)) != 0;
+        }
 
         public bool IsAllredayConfig
         {
-            get { return (Gas_model.Channel_status_l & (1 << 0)) == 0; }
-            //get { return false; }
+            get
+            {
+                return get_bit_bool(((UInt32)Gas_model.Channel_status_h << 16)
+                | (Gas_model.Channel_status_l), 0);
+            }
         }
+        public bool Is_Temperature_concentration_config
+        {
+            get
+            {
+                return get_bit_bool(((UInt32)Gas_model.Channel_status_h << 16)
+| (Gas_model.Channel_status_l), 1);
+            }
+        }
+        public bool Is_bd0_config
+        {
+            get
+            {
+                return get_bit_bool(((UInt32)Gas_model.Channel_status_h << 16)
+                | (Gas_model.Channel_status_l), 2);
+            }
+        }
+        public bool Is_bd1_config
+        {
+            get
+            {
+                return get_bit_bool(((UInt32)Gas_model.Channel_status_h << 16)
+                | (Gas_model.Channel_status_l), 3);
+            }
+        }
+        public bool Is_sensor_ok
+        {
+            get
+            {
+                return get_bit_bool(((UInt32)Gas_model.Channel_status_h << 16)
+                | (Gas_model.Channel_status_l), 4);
+            }
+        }
+        public bool Is_powerOn_init
+        {
+            get
+            {
+                return get_bit_bool(((UInt32)Gas_model.Channel_status_h << 16)
+                | (Gas_model.Channel_status_l), 5);
+            }
+        }
+        public bool Is_config_buckup
+        {
+            get
+            {
+                return get_bit_bool(((UInt32)Gas_model.Channel_status_h << 16)
+                | (Gas_model.Channel_status_l), 6);
+            }
+        }
+        public bool Is_alarm1_triggered
+        {
+            get
+            {
+                return get_bit_bool(((UInt32)Gas_model.Channel_status_h << 16)
+                | (Gas_model.Channel_status_l), 7);
+            }
+        }
+        public bool Is_alarm2_triggered
+        {
+            get
+            {
+                return get_bit_bool(((UInt32)Gas_model.Channel_status_h << 16)
+                | (Gas_model.Channel_status_l), 8);
+            }
+        }
+        #endregion
+
+        #region 实时参数
+
+
         public float Gas_float_value
         {
             get
             {
                 UInt32 tv = ((UInt32)Gas_model.Gas_concentration_float_h << 16) | ((UInt32)Gas_model.Gas_concentration_float_l);
-                //byte[] b = new byte[4];
-                //b[0] = (byte)(Gas_model.Gas_concentration_float_h & 0xff00);
-                //b[1] = (byte)(Gas_model.Gas_concentration_float_h & 0x00ff);
-                //b[2] = (byte)(Gas_model.Gas_concentration_float_l & 0xff00);
-                //b[3] = (byte)(Gas_model.Gas_concentration_float_l & 0x00ff);
                 return BitConverter.ToSingle(BitConverter.GetBytes(tv), 0);
             }
         }
@@ -79,6 +152,27 @@ namespace gas_sensor.ViewModel
                 return Gas_model.Gas_concentration_h << 16 | Gas_model.Gas_concentration_l;
             }
         }
+        public int Gas_adc_value
+        {
+            get
+            {
+                return (int)Gas_model.Gas_adc_value;
+            }
+        }
+        public float Temperature_value
+        {
+            get { return (float)((Gas_model.Temperature_h << 16) | (Gas_model.Temperature_l)) / 1000; }
+        }
+        public int Temperature_adc_value
+        {
+            get { return (int)Gas_model.Temperature_adc_value; }
 
+        }
+
+
+
+
+
+        #endregion
     }
 }
